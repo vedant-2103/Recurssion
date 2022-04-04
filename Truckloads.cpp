02cf653e46c9ccd6fbb819cec40483e47b74700c
VEDANT SINGH <vedu.arpit@gmail.com>
2022-04-04 23:13:15 +0930

    Add files via upload

--- Truckloads.cpp
+++ Truckloads.cpp
@@ -6,14 +6,15 @@ Truckloads::Truckloads(){}
 
 int Truckloads::numTrucks(int numCrates,int loadSize)
 {
-    if(numCrates<=loadSize)
-    {
-        return 1;
-    }
     if(numCrates<=0 || loadSize<=0)
     {
         return 0;
     }
+    if(numCrates<=loadSize)
+    {
+        return 1;
+    }
+    
     else
     {
         int a=numCrates/2;
